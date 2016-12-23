1651a20a79ff9a9071b6cfc5044eee20021f6e08
ReneNeubert <rene.neubert@softcake.og>
2016-12-23 13:45:28 +0100

    feat: sonarqube null

--- template-client/src/main/java/com/netflix/template/client/TalkClient.java
+++ template-client/src/main/java/com/netflix/template/client/TalkClient.java
@@ -55,7 +55,7 @@ public class TalkClient implements Conversation {
         String url = "http://localhost:8080/template-server/rest";
         TalkClient remote = new TalkClient(url);
        System.out.println(remote.greeting().getWhole());
-      //  System.out.println(remote.farewell().getWhole());
+        System.out.println(remote.farewell().getWhole());
 
 
     }
