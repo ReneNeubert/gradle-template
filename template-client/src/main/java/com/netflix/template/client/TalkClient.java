f10d141e34e705dab6827b2169ef50dfe828f3f5
ReneNeubert <rene.neubert@softcake.og>
2016-12-23 11:49:31 +0100

    fix: some changes

--- template-client/src/main/java/com/netflix/template/client/TalkClient.java
+++ template-client/src/main/java/com/netflix/template/client/TalkClient.java
@@ -54,7 +54,7 @@ public class TalkClient implements Conversation {
 
         String url = "http://localhost:8080/template-server/rest";
         TalkClient remote = new TalkClient(url);
-//        System.out.println(remote.greeting().getWhole());
+       System.out.println(remote.greeting().getWhole());
         System.out.println(remote.farewell().getWhole());
 
 
